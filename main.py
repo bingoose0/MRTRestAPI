d8f29f139ec84781a1365eb05ff3bb0d267ab72f
aindrigo <adamindrigo@gmail.com>
2022-05-15 04:04:24 -0400

    add players/username

--- main.py
+++ main.py
@@ -28,7 +28,23 @@ async def players(request: web.BaseRequest):
     player_data = [p.dict for p in players]
 
     return json_response(player_data)
+
+@router.get("/players/{username}")
+async def players_get(request: web.BaseRequest):
+    url = f"{config['dyn_url']}/standalone/dynmap_new.json"
+
+    resp = await request_json(url)
+    username = request.match_info['username']
+    raw_players = resp['players']
+
+    players = [Player(DynmapPlayer(d)) for d in raw_players] # Not just DynmapPlayer cuz we need the dict
+
+    print(username)
+    for p in players:
+        if p.username == username:
+            return json_response(p.dict)
     
+    return web.Response(status=404)
 
 app.add_routes(router)
 web.run_app(app)
